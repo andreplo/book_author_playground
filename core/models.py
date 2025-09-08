3f8f0efed33f1cf7fe4b7f557159e82cb66eba08
andreplo <andreplo@live.com>
2025-09-08 21:37:03 +0300

    initial commit

---
+++ core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    published_at = models.DateField()
+
+    def __str__(self):
+        return self.title
+
