6e95a5b005b90fa1b3d9e4e6b0174e430f099ec1
andreplo <andreplo@live.com>
2025-09-09 12:19:08 +0300

    author view and template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.core.cache import cache
+from .models import Author
 # Create your views here.
+
+def author_list(request):
+    authors = cache.get("authors")
+    if not authors:
+        authors = list(Author.objects.prefetch_related("books").all())
+        cache.set("authors", authors, timeout=30)  # cache for 30s
+    return render(request, "core/author_list.html", {"authors": authors})
